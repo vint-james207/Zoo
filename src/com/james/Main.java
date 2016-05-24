e15bc98916816a595de29bac794dc1e286b7b2f2
jamesyburr <vint.james@gmail.com>
2016-05-24 11:23:37 -0400

    Add factory method

--- src/com/james/Main.java
+++ src/com/james/Main.java
@@ -3,9 +3,9 @@ package com.james;
 public class Main {
 
     public static void main(String[] args) {
-	    Dog d = new Dog();
-        Snake s = new Snake();
-        Hawk h = new Hawk();
+        Animal d = createAnimal("Dog");
+        Animal s = createAnimal("Snake");
+        Animal h = createAnimal("Hawk");
 
         d.makeSound();
         s.makeSound();
@@ -14,5 +14,17 @@ public class Main {
         System.out.println(d);
         System.out.println(s);
         System.out.println(h);
+    }
+
+    public static Animal createAnimal(String name) {
+        switch (name) {
+            case "Dog":
+                return new Dog();
+            case "Snake":
+                return new Snake();
+            case "Hawk":
+                return new Hawk();
+        }
+        return new Animal();
     }
 }
